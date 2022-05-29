04f314984a4ac4833acef840833c8d994fd9c874
Тхе Зунг Буй <43087816+Bd0ptimus@users.noreply.github.com>
2022-05-29 16:09:28 +0300

    next

--- main.py
+++ main.py
@@ -6,14 +6,11 @@ import soundfile as sf
 import ffmpeg
 import config
 import find_topics
+import codecs
 
 from telebot import types 
 #from pydub import AudioSegment
-'''
-class userSayingObj:
-    userMessage=""
-    def __init__(self, userMessageIn):
-        self.userMessage = userMessageIn'''
+
 
 TOKEN = str(config.Config().telegram_token())
 API_KEY = os.getenv(TOKEN)
@@ -23,14 +20,21 @@ bot = telebot.TeleBot(TOKEN)
 def test(message):
     bot.reply_to(message, "123")
 
+'''
 @bot.message_handler(commands = ['hello'])
 def hello(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
     button1 = types.KeyboardButton("Post it somewhere")
     markup.add(button1)
     bot.send_message(message.chat.id, "Hello", reply_markup = markup)
+'''
 
+#write text in txt
+def write_file(string):
+    with codecs.open("txt_store/customer_doc.txt", "a", "utf-8") as stream:   # or utf-8
+        stream.write(string)
 
+#Prepare for voice
 @bot.message_handler(content_types=['voice', 'text'])
 def voice_processing(message):
     if message.content_type == 'voice':
@@ -42,8 +46,11 @@ def voice_processing(message):
         voice_recog.store_voice_file()
         text_result = voice_recog.yandex_voice_recognition()
         bot.send_message(message.chat.id, "#от_заказчика\n"+ text_result )
-        print("Go to prepare for searching")
-        find_topics.prepare_data_for_searching(text_result)
+        #print("Go to prepare for searching")
+        #find_topics.prepare_data_for_searching(text_result)
+        write_file("------------------------------------\n"
+        + text_result+'\n')
+
     else:
         print('text!')
     
--- voice_recog.py
+++ voice_recog.py
@@ -4,9 +4,10 @@ import time
 import boto3
 import config
 
-
+#Store voice file to yandex cloud storage
 def store_voice_file():
     session = boto3.session.Session()
+    #set up section
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net',
@@ -14,14 +15,21 @@ def store_voice_file():
         aws_secret_access_key = str(config.Config().aws_access_secret_key())
 
     )
+    #create file
     s3.put_object(Bucket='voiceconvert', Key='voice.ogg', StorageClass='STANDARD')
+    #upload 
     s3.upload_file('voice_store/voice.ogg', 'voiceconvert','voice.ogg')
 
 
+#Calling Yandex Speech Kit Recognition
 def yandex_voice_recognition():
+    #setup key
     result=""
     key = str(config.Config().yandex_speech_key())
+    #set up url
+    #url file was uploaded
     filelink = 'https://storage.yandexcloud.net/voiceconvert/voice.ogg'
+    #url calling
     POST = "https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize"
     body ={
         "config": {
@@ -33,10 +41,13 @@ def yandex_voice_recognition():
             "uri": filelink
         }
     }
+    #prepare header
     header = {'Authorization': 'Api-Key {}'.format(key)}
+    #request
     req = requests.post(POST, headers=header, json=body)
     data = req.json()
     print(data)
+    #take id of processs
     id = data['id']
     while True: 
         #"#от_заказчика\n"
@@ -46,12 +57,13 @@ def yandex_voice_recognition():
         req = req.json()
         if req['done']: break
         print("In translating")
+    #prepare bind data returned
     print("Текст:")
+    #read each sentence
     for chunk in req['response']['chunks']:
         result =result + chunk['alternatives'][0]['text'] + '. '
     print(result)
+    
     return result
 
-store_voice_file()
-yandex_voice_recognition()
 
